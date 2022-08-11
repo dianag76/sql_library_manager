b212007b9bff78b4d44b0e12895568bc11d069f3
dianag76 <dianag766@gmail.com>
2022-08-10 20:25:49 -0700

    Troubleshooting routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const {sequelize} = require.('.models');
+const {sequelize} = require('.models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,20 +20,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 //test cobbection to the data base and sync the model 
+app.use('/indexRouter');
 (async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection to the database successful!');
-    console.log('Synchronizing the models with the database...');
     await sequelize.sync();
   } catch(error) {
     console.error('Error connecting to the database: ', error);
     }
 })();
+
+app.use('/userRoutes');
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const Book = require("../models").Book;
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ const book = await Book.findAll();
+ res.json({books, title:"Books"})
 });
+
 
 module.exports = router;
