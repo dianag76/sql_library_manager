8a0fc5b4b6aa97d9850f1eb3c85d7a47a562cb26
dianag76 <dianag766@gmail.com>
2022-08-12 12:10:56 -0700

    working on route validation

--- app.js
+++ app.js
@@ -21,10 +21,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware for serving static files 
-app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 
@@ -43,21 +41,6 @@ app.get('/routes', (req, res, next) => {
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 });
-
-// Handling errors
-// app.use((err, req, res, next) => {
-//     if(err){
-//         const err = new Error("Oops.Page not found")
-//         err.status = 404;
-//         console.log(err);
-//         next(err);
-//     }
-// });
-
-// 
-
-
-
 
 
 //test connection to the data base and sync the model 
@@ -96,19 +79,6 @@ app.use((err, req, res, next) => {
 });
 
 
-// // app.use((req, res, next) => {
-//      next(createError(404));
 
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,97 @@
 var express = require('express');
+const { route } = require('../app');
 var router = express.Router();
 const Book = require("../models").Book;//usually singular 
 
 
 
 /* GET home page. */
-// router.get('/', async function(req, res, next) {
-//  const books = await Book.findAll();
-//  res.json(books)
-// });
 
-
-router.get('/', (req,res,next) =>{
-    res.redirect('/book')
+router.get("/", (req, res, next) => {
+  res.redirect("/books");
 });
 
+//shows full list of books
+router.get("/books", async function (req, res, next) {
+  const books = await Book.findAll();
+  res.json(books);
+});
 
-//     console.log ('Custom error route called');
-//     const err = new Error();
-//     err.message = 'Custom 500 error route thrown';
-//     err.status = 500; 
-//     throw err;
-// if (index[req.params.id]){
-//     res.render('index');
-// } else {
-//     const err = new Error();
-//     err.status = 404;
-//     err.message = `Looks like the entry you requested doesn't exist.`
-//     next (err);
-// }
-// });
+// get /books/new - Shows the create new book form
+router.get('/books/new', (req,res) => {
+  res.render('new-book',{book: Book.build(), title: 'New Book'})
+});
+
+// post /books/new - Posts a new book to the database
+router.post('/books/new', (async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + book.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      article = await Book.build(req.body);
+      res.render('new-book', { book, errors, title: 'New Book' })
+    } else {
+      throw error;
+    }  
+  }
+}));
+
+
+// get /books/:id - Shows book detail form
+router.get('/books/:id', (async(req, res) =>{
+  const book = await Book.findByPk(req.params.id);
+  if(book){
+    res.render('update-book', {book,title: book.title});
+    } else {
+     res.render('page-not-found');
+    }
+}));
+
+// post /books/:id - Updates book info in the database
+router.post(
+  "/:id/edit",
+  asyncHandler(async (req, res) => {
+    let books;
+    try {
+      books = await Book.findByPk(req.params.id);
+      if (article) {
+        await Book.update(req.body);
+        res.redirect("/book/" + book.id);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        books = await Book.build(req.body);
+        book.id = req.params.id;
+        res.render("articles/edit", {
+          book,
+          errors: error.errors,
+          title: "Update Book Information",
+        });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+// post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting
+router.post(
+  "/book:id/delete",
+  asyncHandler(async (req, res) => {
+    const books = await Book.findByPk(req.params.id);
+    if (book) {
+      await article.destroy();
+      res.redirect("/book");
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+
 
 module.exports = router;
